f894163b4d224d719a754d8694ce7a42e3ce439e
Jed <jedtony.jt@gmail.com>
2021-10-31 16:21:38 +0100

    Add award achievement listeners

---
+++ app/Listeners/AwardCommentAchievement.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\CommentWritten;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class AwardCommentAchievement
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(CommentWritten $event)
+    {
+        //
+        // dd($event->comment);
+        // dd('A new comment was created');
+        // Count the number of comments
+        // See if it matches the next comment achievement for the user
+        // If it matches then trigger an achievement event
+        
+    }
+}
---
+++ app/Listeners/AwardLessonAchievement.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\LessonWatched;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class AwardLessonAchievement
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(LessonWatched $event)
+    {
+        //
+        // dd('A new comment was created');
+    }
+}
