f10b127a53468d8aedbe76b9d35492a3aa4836ef
Jed <jedtony.jt@gmail.com>
2021-10-31 16:07:32 +0100

    Create table for storing achievements

--- database/migrations/2021_10_30_031303_create_achievements_table.php
+++ database/migrations/2021_10_30_031303_create_achievements_table.php
@@ -15,12 +15,13 @@ class CreateAchievementsTable extends Migration
     {
         Schema::create('achievements', function (Blueprint $table) {
             $table->id();
+            $table->string('slug');
             $table->string('title');
             $table->string('description');
             $table->enum('type', ['lesson', 'comment']);
             $table->integer('condition')
                 ->comment('This is the amount of lesson or comments to be made to unlock this achievement');
-            $table->integer('next_step_id')->nullable();
+            $table->integer('next_achievement_id')->nullable();
             $table->timestamps();
         });
     }
